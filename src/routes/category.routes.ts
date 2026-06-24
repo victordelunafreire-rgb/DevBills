e658cb0a6294425d8c8d808a9da8fc3b818163eb
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-06-24 16:17:38 -0300

    feat: finished configuration category routes

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -2,7 +2,7 @@ import type { FastifyInstance } from 'fastify';
 import { getCategories } from '../controllers/category.controller.js';
 
 const categoryRoutes = async (fastify: FastifyInstance): Promise<void> => {
-	fastify.get('./', getCategories);
+	fastify.get('/', getCategories);
 };
 
 export default categoryRoutes;
