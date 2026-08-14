890f689169a72e7f3fcbc3d327911123fce13274
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-08-14 00:58:07 -0300

    feat: create the button component

--- interface/src/pages/Home.tsx
+++ interface/src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import Button from '../components/Button';
+
 const Home = () => {
 	return (
 		<div>
 			<h2>Home</h2>
+			<Button variant="outline" isLoading>
+				Clique em mim
+			</Button>
 		</div>
 	);
 };
