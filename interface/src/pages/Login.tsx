3cc6298b7dcbc3695fb8de848c29921d87bf2ed2
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-08-16 12:36:54 -0300

    feat: begin to develope the login page

---
+++ interface/src/pages/Login.tsx
@@ -0,0 +1,40 @@
+import GoogleLoginButton from '../components/GoogleLoginButton';
+
+const Login = () => {
+	return (
+		<div className="min-h-screen flex items-center justify-center bg-gray-200 py-12 px-4 sm:px-6 lg:px-8">
+			<div className="max-w-md w-full space-y-8">
+				<header>
+					<h1 className="text-center text-3xl font-extrabold text-gray-900">
+						DevBills
+					</h1>
+					<p className="mt-2 text-center text-sm text-gray-600">
+						Gerencie suas finanças de forma simples e eficiente
+					</p>
+				</header>
+
+				<main className="mt-8 bg-white py-8 px-4 shadow-md rounded-lg sm:px-10 space-y-6">
+					<section className="mb-6">
+						<h2 className="text-lg font-medium text-gray-900">
+							Faça login para continuar
+						</h2>
+						<p className="mt-1 text-sm text-gray-600">
+							Acesse sua conta para começar a gerenciar suas finanças
+						</p>
+					</section>
+
+					<GoogleLoginButton />
+
+					<footer className="mt-6">
+						<p className="mt-1 text-sm text-gray-600">
+							Ao fazer login, você concorda com nossos termos de uso e política
+							de privacidade.
+						</p>
+					</footer>
+				</main>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
--- interface/src/routes/index.tsx
+++ interface/src/routes/index.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
 
 const AppRoutes = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+
 				<Route path="*" element={<h2>Página não encontrada</h2>} />
 			</Routes>
 		</BrowserRouter>
