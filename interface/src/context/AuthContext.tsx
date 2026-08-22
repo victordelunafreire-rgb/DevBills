97c398777451ffb557f3cf591fbb5e9e24f67105
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-08-22 19:31:24 -0300

    feat: added token to the api call

--- interface/src/context/AuthContext.tsx
+++ interface/src/context/AuthContext.tsx
@@ -32,6 +32,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 		const unsubscribe = onAuthStateChanged(
 			firebaseAuth,
 			(user) => {
+				console.log(user);
 				if (user) {
 					setAuthState({
 						user: {
--- interface/src/services/api.ts
+++ interface/src/services/api.ts
@@ -1,6 +1,29 @@
-import axios, { type AxiosInstance } from 'axios';
+import axios, {
+	type AxiosInstance,
+	type InternalAxiosRequestConfig,
+} from 'axios';
+import { firebaseAuth } from '../config/firebase';
 
 export const api: AxiosInstance = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
 	timeout: 10000, // 10 seconds
 });
+
+api.interceptors.request.use(
+	async (
+		config: InternalAxiosRequestConfig,
+	): Promise<InternalAxiosRequestConfig> => {
+		const user = firebaseAuth.currentUser;
+
+		if (user) {
+			try {
+				const token = await user.getIdToken();
+				config.headers.set('Authorization', `Bearer ${token}`);
+			} catch (err) {
+				console.error(err, 'Erro ao obter token do usuário no Firebase');
+			}
+		}
+
+		return config;
+	},
+);
