68ee92073a706c524a46bd0cb068f793e9054ce2
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-09-10 02:28:32 -0300

    feat: finished the responsive part of the header; adding menu button and css

--- interface/src/components/Header.tsx
+++ interface/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Activity, LogIn, LogOut } from 'lucide-react';
+import { Activity, LogIn, LogOut, Menu, X } from 'lucide-react';
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router';
 import { UseAuth } from '../context/AuthContext';
@@ -21,7 +21,12 @@ const Header = () => {
 	];
 
 	const logOff = (): void => {
+		setIsOpen(false);
 		signOut();
+	};
+
+	const changeMenu = (): void => {
+		setIsOpen(!isOpen);
 	};
 
 	const renderAvatar = () => {
@@ -102,8 +107,68 @@ const Header = () => {
 							</Link>
 						)}
 					</div>
+
+					{/* MOBILE BUTTON */}
+					<div className="md:hidden flex items-center">
+						<button
+							type="button"
+							className="text-gray-400 p-2 rounded-lg hover:bg-gray-800 transition-colors"
+							onClick={changeMenu}
+						>
+							{isOpen ? <X size={24} /> : <Menu size={24} />}
+						</button>
+					</div>
 				</div>
 			</div>
+
+			{isOpen && (
+				<div>
+					<div>
+						{isAuthenticated ? (
+							<>
+								<nav className="space-y-1">
+									{navLink.map((link) => (
+										<Link
+											to={link.path}
+											key={link.path}
+											className={`block p-5 rounded-lg ${
+												pathname === link.path
+													? 'bg-gray-800 text-primary-500 font-medium'
+													: 'text-gray-400 hover:bg-gray-800 hover:text-primary-500'
+											}`}
+											onClick={() => setIsOpen(false)}
+										>
+											{link.name}
+										</Link>
+									))}
+								</nav>
+
+								<div className="flex items-center justify-between p-4 border-t border-gray-700">
+									<div className="flex items-center space-x-2">
+										{renderAvatar()}
+										<span>{authState.user?.displayName}</span>
+									</div>
+									<button
+										type="button"
+										onClick={logOff}
+										className="cursor-pointer text-gray-400 hover:text-red-700 p-2 rounded-full hover:bg-red-200 transition-colors"
+									>
+										<LogOut size={20} />
+									</button>
+								</div>
+							</>
+						) : (
+							<Link
+								to="/login"
+								className="bg-primary-500 text-gray-800 font-semibold px-5 py-2.5 rounded-2xl flex items-center justify-center hover:bg-primary-600"
+								onClick={() => setIsOpen(false)}
+							>
+								Entrar
+							</Link>
+						)}
+					</div>
+				</div>
+			)}
 		</header>
 	);
 };
