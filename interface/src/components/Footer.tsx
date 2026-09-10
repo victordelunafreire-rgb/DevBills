e59686d81a8c139889cffbe11d9f468d3a820cd1
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-09-10 00:46:14 -0300

    feat: added final touches to footer with css styles

--- interface/src/components/Footer.tsx
+++ interface/src/components/Footer.tsx
@@ -1,8 +1,14 @@
 const Footer = () => {
+	const currentYear = new Date().getFullYear();
 	return (
-		<div>
-			<h1>Footer</h1>
-		</div>
+		<footer className="bg-gray-800 border-t border-gray-700 py-4">
+			<div className="container-app">
+				<p className="text-sm text-gray-400 text-center">
+					DevBills <strong>®</strong> {currentYear} - Desenvolvido por{' '}
+					<strong>Victor de Luna</strong>
+				</p>
+			</div>
+		</footer>
 	);
 };
 
